d3292e6cdcc54b91d6736de8b4ae97280066c8ed
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-12 12:37:06 -0700

    added the first conditions to create a new reservation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,49 +2,66 @@
  * List handler for reservation resources
  */
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const nextId = require("../utils/nextId");
+// const nextId = require("../utils/nextId");
 const service = require("./reservations.service");
+const hasProperties = require("../errors/hasProperties");
 
-// const reservations = require();
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+];
 
-//--functions--//
-// async function list(req, res) {
-//   const data = await service.list();
-//   if (data) {
-//     res.status(200).json({ data: data });
-//   } else {
-//     nextId({
-//       status: 404,
-//       mesage: "There are no reservations",
-//     });
-//   }
-// }
+//----Middleware----//
+function hasValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
+
+const hasRequiredProperties = hasProperties(...VALID_PROPERTIES);
+
+function hasValidQuery(req, res, next) {
+  const { date, mobile_number } = req.query;
+  if (!date && !mobile_number) {
+    return next({
+      status: 400,
+      message: `Either a ?date or ?mobile_number query is needed`,
+    });
+  }
+
+  next();
+}
+// function validateProperty()
+
+//Function to get all reservations
 async function list(req, res) {
   res.status(200).json({ data: await service.list() });
 }
 
-// function create(req, res, next) {
-//   const {
-//     data: {
-//       first_name,
-//       last_name,
-//       mobile_number,
-//       reservation_date,
-//       reservation_time,
-//       people,
-//     } = {},
-//   } = req.body;
-
-//   const newReservation = {
-//     first_name: first_name,
-//     last_name: last_name,
-//     mobile_number: mobile_number,
-//     reservation_date: reservation_date,
-//     reservation_time: reservation_time,
-//     people: people,
-//   };
-//}
+async function create(req, res, next) {
+  const data = await service.create(req.body.data);
+  res.status(201).json({ data });
+}
 
 module.exports = {
-  list: asyncErrorBoundary(list),
+  list: [hasValidQuery, asyncErrorBoundary(list)],
+  create: [
+    hasValidProperties,
+    hasRequiredProperties,
+    asyncErrorBoundary(create),
+  ],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,17 @@
 const knex = require("../db/connection");
 
+function create(newReservation) {
+  return knex("reservations")
+    .insert(newReservation)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
 function list() {
   return knex("reservations").select("*");
 }
 
 module.exports = {
   list,
+  create,
 };
