075261ddc73683910a91fc876acc7e04c81109a8
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-16 11:42:35 -0700

    Moved the read function from tables.service to reservations.service.

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -11,12 +11,6 @@ function list() {
   return knex("tables").select("*").orderBy("table_name");
 }
 
-function read(reservation_id) {
-  return knex("reservations")
-    .select("*")
-    .where({ reservation_id: reservation_id })
-    .first();
-}
 module.exports = {
   list,
   create,
