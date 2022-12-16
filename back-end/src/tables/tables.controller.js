ce3484f4480ba322c4631583fd90570d26d8873f
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-16 11:23:52 -0700

    Fixed the 'capacity' validation to check if the value is a number.

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,5 +1,42 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./tables.service");
+const hasProperties = require("../errors/hasProperties");
+
+//----Middleware----//
+const REQUIRED_PROPERTIES = ["table_name", "capacity"];
+const hasRequiredProperties = hasProperties(...REQUIRED_PROPERTIES);
+
+function validateTableName(req, res, next) {
+  const { table_name } = req.body.data;
+
+  if (!table_name) {
+    return next({
+      status: 400,
+      message: "table_name is missing",
+    });
+  }
+
+  if (!(table_name.length > 1)) {
+    next({
+      status: 400,
+      message: "table_name must be more than 1 character.",
+    });
+  }
+  next();
+}
+
+function validateCapacity(req, res, next) {
+  const { capacity } = req.body.data;
+  console.log(capacity);
+
+  if (typeof capacity !== "number") {
+    return next({
+      status: 400,
+      message: "capacity must be a number.",
+    });
+  }
+  next();
+}
 
 //----Functions----//
 async function list(req, res) {
@@ -12,5 +49,10 @@ async function create(req, res) {
 
 module.exports = {
   list: [asyncErrorBoundary(list)],
-  create: [asyncErrorBoundary(create)],
+  create: [
+    hasRequiredProperties,
+    validateTableName,
+    validateCapacity,
+    asyncErrorBoundary(create),
+  ],
 };
