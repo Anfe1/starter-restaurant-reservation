1102c0bf12cdc24f795889d4e744089f7561acf9
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-20 16:04:03 -0700

    Added a status to the tables table.

--- back-end/src/db/migrations/20221215131258_createTablesTable.js
+++ back-end/src/db/migrations/20221215131258_createTablesTable.js
@@ -3,6 +3,7 @@ exports.up = function (knex) {
     table.increments("table_id").primary();
     table.string("table_name").notNullable();
     table.integer("capacity").notNullable();
+    table.string("status").notNullable().defaultTo("booked");
     table.integer("reservation_id").unsigned().unique();
     table
       .foreign("reservation_id")
