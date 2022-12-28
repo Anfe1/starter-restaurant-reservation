d75046d138e30870218358c34254b87d8a0f048d
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-27 17:25:21 -0700

    Added the status parameter for the reservation update function.

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -11,16 +11,20 @@ function list() {
   return knex("tables").select("*").orderBy("table_name");
 }
 
-function read(reservation_id) {
+function readReservationById(reservation_id) {
+  return knex("reservations").select("*").where({ reservation_id }).first();
+}
+
+function readTableId(table_id) {
   return knex("tables")
     .select("*")
-    .where({ reservation_id: reservation_id })
-    .then((result) => result[0]);
+    .where({ table_id: table_id })
+    .then((record) => record[0]);
 }
 
 //function update a table with a new reservation id and a seated status
-function updateReservationTable(table_id, reservation_id, status) {
-  return knex("table")
+function updateTableReservationIdStatus(table_id, reservation_id, status) {
+  return knex("tables")
     .select("*")
     .where({ table_id: table_id })
     .update({ reservation_id: reservation_id, status: status });
@@ -29,6 +33,7 @@ function updateReservationTable(table_id, reservation_id, status) {
 module.exports = {
   list,
   create,
-  updateReservationTable,
-  read,
+  updateTableReservationIdStatus,
+  readReservationById,
+  readTableId,
 };
