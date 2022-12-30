75abb0f43dd04b18e0201fd7c11232a13148ce39
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-29 18:47:03 -0700

    Addeded update reservation and table status function.

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -15,6 +15,13 @@ function readReservationById(reservation_id) {
   return knex("reservations").select("*").where({ reservation_id }).first();
 }
 
+function readTableByRes(reservation_id) {
+  return knex("tables")
+    .where({ reservation_id })
+    .whereExists(knex.select("*").from("tables").where({ reservation_id }))
+    .then((result) => result[0]);
+}
+
 function readTableId(table_id) {
   return knex("tables")
     .select("*")
@@ -23,11 +30,35 @@ function readTableId(table_id) {
 }
 
 //function update a table with a new reservation id and a seated status
-function updateTableReservationIdStatus(table_id, reservation_id, status) {
-  return knex("tables")
-    .select("*")
-    .where({ table_id: table_id })
-    .update({ reservation_id: reservation_id, status: status });
+async function updateTableReservationIdStatus(reservation_id, table_id) {
+  const trx = await knex.transaction();
+  return trx("tables")
+    .where({ table_id })
+    .update(
+      {
+        reservation_id: reservation_id,
+        status: "occupied",
+      },
+      "*"
+    )
+    .then(() =>
+      trx("reservations").where({ reservation_id }).update({ status: "seated" })
+    )
+    .then(trx.commit)
+    .catch(trx.rollback);
+}
+
+function destroyTable(table_id, reservation_id) {
+  return knex.transaction(function (trx) {
+    return trx("tables")
+      .where({ table_id: table_id })
+      .update({ reservation_id: null })
+      .then(() => {
+        return trx("reservations")
+          .where({ reservation_id })
+          .update({ status: "finished" });
+      });
+  });
 }
 
 module.exports = {
@@ -36,4 +67,6 @@ module.exports = {
   updateTableReservationIdStatus,
   readReservationById,
   readTableId,
+  destroyTable,
+  readTableByRes,
 };
