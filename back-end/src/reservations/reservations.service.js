0b488c41d6f4832699804c862871f0cb7ba7612c
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-12-29 18:39:49 -0700

    Added functions to update the reservation.

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -11,6 +11,7 @@ function listDate(reservation_date) {
   return knex("reservations")
     .select("*")
     .where({ reservation_date })
+    .whereNotIn("status", ["finished", "canceled"])
     .orderBy("reservation_time");
 }
 
@@ -24,9 +25,58 @@ function updateReservationStatus(reservation_id, status) {
     .update({ status: status }, "*");
 }
 
+function searchByPhoneNumber(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
+
+function updateReservationById(updatedReservation) {
+  return knex("reservations")
+    .where({ reservation_id: updatedReservation.reservation_id })
+    .whereNot({ status: "finished" })
+    .update(updatedReservation, "*")
+    .then((updatedRecord) => updatedRecord[0]);
+}
+
+async function updateReservation(reservation) {
+  const {
+    reservation_id,
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+  } = reservation;
+  return knex("reservations").where({ reservation_id }).update(
+    {
+      first_name: first_name,
+      last_name: last_name,
+      mobile_number: mobile_number,
+      reservation_date: reservation_date,
+      reservation_time: reservation_time,
+      people: people,
+    },
+    [
+      "first_name",
+      "last_name",
+      "mobile_number",
+      "people",
+      "reservation_date",
+      "reservation_time",
+    ]
+  );
+}
 module.exports = {
   listDate,
   create,
   readReservationId,
   updateReservationStatus,
+  searchByPhoneNumber,
+  updateReservationById,
+  updateReservation,
 };
